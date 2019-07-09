ba10ded9a6c668cea63efe7d8a5f0a7feae2de61
sheikhsuleman123 <mssheikh333@gmail.com>
2019-07-09 19:33:38 +0500

    Initial starter code setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import './core.css';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper fixed"> 
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <div className="nav fixed"> 
+        <ul className="navigation">
+            <li><a href="#" className=""></a>  </li> 
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Navigation;
