86ef055e1899ebd412ef77b9234f185f0f1f119e
sheikhsuleman123 <mssheikh333@gmail.com>
2019-07-10 17:53:37 +0500

    navigation styling is done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React,{Component} from 'react';
 import './core.css';
 import './App.css';
 
+import Navigation from './components/Navigation';
+
 class App extends React.Component {
   render() {
     return (
       <div className="wrapper fixed"> 
-
+          <Navigation />
       </div>
     )
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,43 @@
 import React,{Component} from 'react';
+import { styles } from 'ansi-colors';
 
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab: "home",
+      
+    }
+  }
+
+  switchTab (newTab)   {
+    this.setState({ 
+      tab : newTab
+      
+    })
+    
+  }
   render() {
+    var self = this;
+    console.log(self.state);
     return (
-      <div className="nav fixed"> 
-        <ul className="navigation">
-            <li><a href="#" className=""></a>  </li> 
+
+        <ul className="navigation fixed">
+            <li><a onClick={() =>{
+              self.switchTab("home")
+            } } href="javascript:;"  className={"anim noul bl fas fa-home rel" + (self.state.tab === "home" ? " on" : "")} ><div className="tip abs fontn">Home</div> </a> </li>
+            <li><a onClick={() =>{
+              self.switchTab("profile")
+            } } href="javascript:;"  className={"anim noul bl fas fa-user rel" + (self.state.tab === "profile" ? " on" : "")}><div className="tip abs fontn">Profile</div></a> </li>
+            <li><a onClick={() =>{
+              self.switchTab("gallery")
+            } } href="javascript:;"  className={"anim noul bl fas fa-images rel"+ (self.state.tab === "gallery" ? " on" : "")}><div className="tip abs fontn">Gallery</div></a> </li>
+            <li><a onClick={() =>{
+              self.switchTab("contact")
+            } } href="javascript:;"  className={"anim noul bl fas fa-envelope rel" + (self.state.tab === "contact" ? " on" : "")}><div className="tip abs fontn">Contact</div></a> </li> 
         </ul>
-      </div>
     )
+    console.log(self.state);
   }
 }
 
